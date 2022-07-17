21bbe0721b7afaec85f6f554468840f2065f45cf
Fatemeh Fazli <fatemehfazli82@gmail.com>
2022-07-17 09:25:20 +0430

    Added PostgreSQL environment variable and psycopg2.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# import os
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -6,11 +6,11 @@ from datetime import timedelta
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item,ItemList
-from resources.store import Store, StoreList
+from resources.store import Store, StoreLists
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 app.secret_key = 'jose'
