437c32f4c3a52a6342228796b6caa36087ddfeb6
Fatemeh Fazli <fatemehfazli82@gmail.com>
2022-07-18 09:57:29 +0430

    Added PostgreSQL environment variable and psycopg2.

--- security.py
+++ security.py
@@ -1,16 +1,3 @@
-# from models.user import UserModel
-# from hmac import compare_digest
-
-# def authenticate(username, password):
-#     user = UserModel.find_by_username(username)
-    
-#     if user and compare_digest(user.password, password):
-#         return user
-
-# def identity(payload):
-#     user_id = payload['identity']
-#     return UserModel.find_by_id(user_id)
-# from werkzeug.security import safe_str_cmp
 from models.user import UserModel
 
 def authenticate(username, password):
